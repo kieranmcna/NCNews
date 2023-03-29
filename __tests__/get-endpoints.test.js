025290fb8ad350880954638e135cbbf491c4fa20
kieranmcna <kieranmcnamara27@gmail.com>
2023-03-29 12:13:00 +0100

    Refactor for Code Review

--- __tests__/get-endpoints.test.js
+++ __tests__/get-endpoints.test.js
@@ -97,45 +97,22 @@ describe("GET /api/articles", () => {
         return request(app)
             .get("/api/articles")
             .expect(200)
-            .then((result) => {
-                expect(Array.isArray(result.body)).toBe(true);
-                expect(Object.keys(result.body[0])).toHaveLength(9);
-                expect(result.body[0]).toEqual({
-                    article_id: 3,
-                    title: 'Eight pug gifs that remind me of mitch',
-                    topic: 'mitch',
-                    author: 'icellusedkars',
-                    body: 'some gifs',
-                    created_at: '2020-11-03T09:12:00.000Z',
-                    votes: 0,
-                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-                    comment_count: '2'
-                });
-                expect(result.body[1]).toEqual({
-                    article_id: 6,
-                    title: 'A',
-                    topic: 'mitch',
-                    author: 'icellusedkars',
-                    body: 'Delicious tin of cat food',
-                    created_at: '2020-10-18T01:00:00.000Z',
-                    votes: 0,
-                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-                    comment_count: '1'
-                });
-                expect(result.body[2]).toEqual({
-                    article_id: 2,
-                    title: 'Sony Vaio; or, The Laptop',
-                    topic: 'mitch',
-                    author: 'icellusedkars',
-                    body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
-                    created_at: '2020-10-16T05:03:00.000Z',
-                    votes: 0,
-                    article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
-                    comment_count: '0'
+            .then(({ body }) => {
+                const articles = body;
+                expect(Array.isArray(articles)).toBe(true);
+                expect(Object.keys(articles[0])).toHaveLength(9);
+                articles.forEach((article) => {
+                    expect(article).toHaveProperty("article_id", expect.any(Number));
+                    expect(article).toHaveProperty("title", expect.any(String));
+                    expect(article).toHaveProperty("topic", expect.any(String));
+                    expect(article).toHaveProperty("author", expect.any(String));
+                    expect(article).toHaveProperty("body", expect.any(String));
+                    expect(article).toHaveProperty("created_at", expect.any(String));
+                    expect(article).toHaveProperty("article_img_url", expect.any(String));
+                    expect(article).toHaveProperty("comment_count", expect.any(String));
                 });
             });
-    });
-
+    })
 
     test("responds with correct comment count for each article", () => {
         return request(app)
