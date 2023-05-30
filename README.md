0ea1cf914509f7062cfcc47490878a31967d959a
kieranmcna <kieranmcnamara27@gmail.com>
2023-05-30 23:07:44 +0930

    CORS Changes

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 To use this repo locally create .env.development & .env.test files with the following contents
 .env.development - PGDATABASE=DEV_DB
-.env.test  - PGDATABASE=DEV_TEST_DB
+.env.test - PGDATABASE=DEV_TEST_DB
 
-- .get /api/topics - Returns Topics with Slugs & their descriptions 
+- .get /api/topics - Returns Topics with Slugs & their descriptions
 - .get /api/articles/:articleId - Returns articles with the given article ID
 - .get /api/articles - Returns a list of all articles
 - .get /api/articles/:articleId/comments - Returns a list of comments for that article
 - .post /api/articles/:articleId/comments - Allows users to post comments
 - .delete /api/comments/:commentID - Allows users to delete comments
--.get /api/users - Shows a list of users
+- .get /api/users - Shows a list of users
--- app.js
+++ app.js
@@ -1,43 +1,42 @@
 const express = require("express");
+const cors = require("cors");
+app.use(cors());
 const app = express();
 const {
-    sendTopics,
-    invalidEndpoint,
-    sendArticleInfo,
-    sendArticles,
-    sendComments,
-    postComments,
-    patchComments,
-    deleteCommentsRequest,
-    sendUsers,
+  sendTopics,
+  invalidEndpoint,
+  sendArticleInfo,
+  sendArticles,
+  sendComments,
+  postComments,
+  patchComments,
+  deleteCommentsRequest,
+  sendUsers,
 } = require("./controller/controller");
 
 app.use(express.json());
 
+app.get("/api/topics/", sendTopics);
 
-app.get("/api/topics/", sendTopics)
+app.get("/api/articles/:articleId", sendArticleInfo);
 
-app.get("/api/articles/:articleId", sendArticleInfo)
+app.get("/api/articles", sendArticles);
 
-app.get("/api/articles", sendArticles)
+app.get("/api/articles/:articleId/comments", sendComments);
 
-app.get("/api/articles/:articleId/comments", sendComments)
+app.post("/api/articles/:articleId/comments", postComments);
 
-app.post("/api/articles/:articleId/comments", postComments)
+app.patch("/api/articles/:articleId", patchComments);
 
-app.patch("/api/articles/:articleId", patchComments)
+app.delete("/api/comments/:commentId", deleteCommentsRequest);
 
-app.delete("/api/comments/:commentId", deleteCommentsRequest)
-
-app.get("/api/users", sendUsers)
-
+app.get("/api/users", sendUsers);
 
 app.use((error, request, response, next) => {
-    if (error.status && error.msg) {
-        response.status(error.status).send({ message: error.msg })
-    }
-})
-app.all('*', invalidEndpoint)
+  if (error.status && error.msg) {
+    response.status(error.status).send({ message: error.msg });
+  }
+});
+app.all("*", invalidEndpoint);
 
-
-module.exports = app
+module.exports = app;
